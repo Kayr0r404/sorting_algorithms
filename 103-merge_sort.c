3815f21e3c5d58ec78e33d650151d001e7ae5673
Kayr0r404 <mahlarekarabo.702@gmail.com>
2023-09-23 18:58:29 +0200

    merge sort implementation

---
+++ 103-merge_sort.c
@@ -0,0 +1,77 @@
+#include "sort.h"
+
+/**
+ * merge_sort - implementation of merge sort, sorting an
+ * array of integers
+ * @array: input array opn integers
+ * @size: the size of the input array
+*/
+
+void merge_sort(int *array, size_t size)
+{
+	size_t mid = size / 2, i, j = 0;
+	int *left_arr, *right_arr;
+
+	if (size == 0 || size == 1 || array == NULL)
+		return;
+
+	left_arr = malloc(sizeof(int) * mid);
+	right_arr = malloc(sizeof(int) * (size - mid));
+
+	/*populate both left and right arrays*/
+	for (i = 0; i < size; i++)
+	{
+		if (i >= mid)
+		{
+			*(right_arr + j) = array[i];
+			j++;
+		}
+		else
+			*(left_arr + i) = array[i];
+	}
+
+	/*call the function/ recurse, first the left then right*/
+	merge_sort(left_arr, mid);
+	printf("merging...\n");
+	merge_sort(right_arr, size - mid);
+
+	merge(left_arr, right_arr, array, mid, size - mid);
+
+	printf("[left]:");
+	print_array(left_arr, mid);
+	printf("[right]:");
+	print_array(right_arr, size - mid);
+	printf("[Done]:");
+	print_array(array, size);
+
+	free(left_arr);
+	free(right_arr);
+}
+
+/**
+ * merge - merge two arrays of integers
+ * @left_arr: input array of integers
+ * @right_arr: input array of integers
+ * @main_arr: destination array
+ * @l_size: the size of left_arr
+ * @r_size: the size of the right_arr
+*/
+void merge(int *left_arr, int *right_arr,
+int *main_arr, size_t l_size, size_t r_size)
+{
+	size_t i = 0, j = 0, k = 0;
+
+	while (i < l_size && j < r_size)
+	{
+		if (left_arr[i] > right_arr[j])
+			main_arr[k++] = right_arr[j++];
+		else
+			main_arr[k++] = left_arr[i++];
+	}
+
+	while (i < l_size)
+		main_arr[k++] = left_arr[i++];
+
+	while (j < r_size)
+		main_arr[k++] = right_arr[j++];
+}
--- sort.h
+++ sort.h
@@ -32,6 +32,7 @@ void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
 void counting_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
+void merge(int *left_arr, int *right_arr, int *main_arr, size_t l_size, size_t r_size);
 void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
